5eaa843f1b1988dce7b1f8109e8092c30c51aab8
Bruno Jimenez <omarjm0910@icloud.com>
2026-07-20 20:55:41 -0600

    refactor

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:photo_mark/widgets/photo_mark_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final ImagePicker picker = ImagePicker();
+  XFile? image;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Photo Mark')),
-      body: const PhotoMarkWidget(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton.icon(
+                onPressed: () {
+                  picker.pickImage(source: ImageSource.camera).then((value) {
+                    setState(() {
+                      image = value;
+                    });
+                  });
+                },
+                icon: const Icon(Icons.add_a_photo),
+                label: const Text('Seleccionar imagen'),
+              ),
+            ],
+          ),
+          (image != null)
+              ? Column(
+                  children: [
+                    PhotoMarkWidget(image: image!),
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          image = null;
+                        });
+                      },
+                      child: const Text('Borrar imagen'),
+                    ),
+                  ],
+                )
+              : const Text('Sin imagen seleccionada'),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/photo_mark_widget.dart
+++ lib/widgets/photo_mark_widget.dart
@@ -3,59 +3,13 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-class PhotoMarkWidget extends StatefulWidget {
-  const PhotoMarkWidget({super.key});
+class PhotoMarkWidget extends StatelessWidget {
+  final XFile image;
 
-  @override
-  State<PhotoMarkWidget> createState() => _PhotoMarkWidgetState();
-}
-
-class _PhotoMarkWidgetState extends State<PhotoMarkWidget> {
-  final ImagePicker? picker = ImagePicker();
-  XFile? image;
-
-  @override
-  void initState() {
-    super.initState();
-  }
+  const PhotoMarkWidget({super.key, required this.image});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton.icon(
-              onPressed: () {
-                picker?.pickImage(source: ImageSource.camera).then((value) {
-                  setState(() {
-                    image = value;
-                  });
-                });
-              },
-              icon: const Icon(Icons.add_a_photo),
-              label: const Text('Seleccionar imagen'),
-            ),
-          ],
-        ),
-        (image != null)
-            ? Column(
-                children: [
-                  Image.file(File(image!.path)),
-                  TextButton(
-                    onPressed: () {
-                      setState(() {
-                        image = null;
-                      });
-                    },
-                    child: const Text('Borrar imagen'),
-                  ),
-                ],
-              )
-            : Text('Sin imagen seleccionada'),
-      ],
-    );
+    return Image.file(File(image.path));
   }
 }
